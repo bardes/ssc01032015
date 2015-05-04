bf9af270ec4541f438b86d7f83df5b4b5c2383f9
bardes <paulobardes@ig.com.br>
2015-05-04 13:53:47 -0300

    Yay

---
+++ t2/src/TTT/TTTMain.java
@@ -0,0 +1,7 @@
+package TTT
+
+public class TTTMain {
+    public static void main(String args[]) {
+        
+    }
+}
---
+++ t2/src/TTT/TTTServer.java
@@ -0,0 +1,13 @@
+package TTT
+
+import java.net.*
+
+public class TTTServer {
+    private Socket clientListner;
+
+    public TTTServer(int port) {
+    }
+
+    public void run() {
+    }
+}
